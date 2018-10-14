943c22b4f4b6fb11d4a11431e659bebedaa1b197
222464 <ericlaukien@gmail.com>
2018-10-14 13:59:32 -0400

    Better defaults + additional actor parameters. (#4)
    * Testing tanh
    
    * Sigmoid again
    
    * Softmax in predictor
    
    * Added whitener
    
    * Whitener changes
    
    * SC-Example
    
    * Modified image encoder norm
    
    * Minor cleanup in kernel
    
    * Sigmoids
    
    * SC and ImEnc improvements
    
    * Reverted sigmoids
    
    * Search scalar
    
    * Fixed search scalar opencl bug
    
    * Tuned example
    
    * Removed search scalar
    
    * Max 0 weight update
    
    * Code cleanup
    
    * Fixed naming of visibleSize
    
    * Small bug fix
    
    * Added missing accessors
    
    * Added missing accessors
    
    * Fixed actor bug
    
    * Actor kernel arg order fix
    
    * Traced
    
    * Better defaults
    
    * Better defaults again
    
    * Separate trace decay for actor
    
    * Added td error clipping
    
    * Better SC defaults

--- source/neo/Actor.h
+++ source/neo/Actor.h
@@ -100,10 +100,20 @@ namespace ogmaneo {
         cl_float _gamma;
 
         /*!
+        \brief Trace decay (lambda)
+        */
+        cl_float _traceDecay;
+
+        /*!
+        \brief TD error clipping
+        */
+        cl_float _tdErrorClip;
+
+        /*!
         \brief Initialize defaults
         */
         Actor()
-        : _alpha(0.01f), _gamma(0.99f)
+        : _alpha(0.01f), _gamma(0.99f), _traceDecay(0.98f), _tdErrorClip(1.0f)
         {}
 
         /*!
