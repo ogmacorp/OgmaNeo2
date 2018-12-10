290087cae3b441f1884305e9c7077399a5efd11e
Eric Laukien <ericlaukien@gmail.com>
2018-12-10 12:54:02 -0500

    Fixes

--- source/neo/Actor.h
+++ source/neo/Actor.h
@@ -133,7 +133,7 @@ namespace ogmaneo {
         \brief Initialize defaults
         */
         Actor()
-        : _alpha(0.1f), _gamma(0.9f), _historyIters(8)
+        : _alpha(0.01f), _gamma(0.9f), _historyIters(8)
         {}
 
         /*!
