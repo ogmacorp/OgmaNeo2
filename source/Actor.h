a74ae52ad15bb651cb24a4e8cae5d02c4aadc500
Eric Laukien <ericlaukien@gmail.com>
2019-05-01 18:52:35 -0400

    Fixes

--- source/Actor.h
+++ source/Actor.h
@@ -70,10 +70,10 @@ public:
 
     Actor()
     :
-    _alpha(0.01f),
-    _beta(0.1f),
+    _alpha(0.02f),
+    _beta(0.2f),
     _gamma(0.98f),
-    _epsilon(0.02f)
+    _epsilon(0.05f)
     {}
 
     void init(
