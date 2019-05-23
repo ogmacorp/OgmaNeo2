10533d4238dcb58e3da496c0c0899548bda1d9b0
Eric Laukien <ericlaukien@gmail.com>
2019-05-23 08:46:42 -0400

    Tuning

--- source/Actor.h
+++ source/Actor.h
@@ -70,10 +70,10 @@ public:
 
     Actor()
     :
-    _alpha(0.02f),
-    _beta(0.2f),
-    _gamma(0.97f),
-    _epsilon(0.02f)
+    _alpha(0.05f),
+    _beta(0.1f),
+    _gamma(0.95f),
+    _epsilon(0.01f)
     {}
 
     void init(
