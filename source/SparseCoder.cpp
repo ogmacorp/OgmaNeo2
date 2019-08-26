e227992802ea1ffb7d352dbca7402027071ca789
Eric Laukien <ericlaukien@gmail.com>
2019-08-26 10:41:10 -0400

    Fixes

--- source/SparseCoder.cpp
+++ source/SparseCoder.cpp
@@ -62,7 +62,7 @@ void SparseCoder::learn(
 
         float sum = vl._weights.multiplyOHVsT(_hiddenCs, visibleIndex, _hiddenSize.z) / std::max(1, vl._visibleCounts[visibleColumnIndex]);
 
-        float delta = _alpha * (target - (sum > 0.0f ? 1.0f + sum : std::exp(sum)));
+        float delta = _alpha * (target - sum);
 
         vl._weights.deltaOHVsT(_hiddenCs, delta, visibleIndex, _hiddenSize.z);
     }
@@ -83,7 +83,7 @@ void SparseCoder::initRandom(
     int numHiddenColumns = _hiddenSize.x * _hiddenSize.y;
     int numHidden = numHiddenColumns * _hiddenSize.z;
 
-    std::uniform_real_distribution<float> weightDist(0.0f, 0.01f);
+    std::uniform_real_distribution<float> weightDist(1.0f, 1.01f);
 
     // Create layers
     for (int vli = 0; vli < _visibleLayers.size(); vli++) {
