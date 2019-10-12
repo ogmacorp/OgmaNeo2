ee05e566e4c19bd62c99f43641786e0d7e83a040
Eric Laukien <ericlaukien@gmail.com>
2019-10-12 11:51:10 -0400

    Fixes

--- source/Predictor.cpp
+++ source/Predictor.cpp
@@ -93,7 +93,7 @@ void Predictor::initRandom(
     int numHiddenColumns = _hiddenSize.x * _hiddenSize.y;
     int numHidden = numHiddenColumns * _hiddenSize.z;
 
-    std::uniform_real_distribution<float> weightDist(-0.001f, 0.001f);
+    std::uniform_real_distribution<float> weightDist(-0.01f, 0.01f);
 
     // Create layers
     for (int vli = 0; vli < _visibleLayers.size(); vli++) {
