ed22c738f18c31b850da0da0d14dfca4809a1eb2
Eric Laukien <ericlaukien@gmail.com>
2019-11-03 12:45:03 -0500

    Recon

--- source/ImageEncoder.cpp
+++ source/ImageEncoder.cpp
@@ -54,7 +54,7 @@ void ImageEncoder::forward(
         for (int hc = 0; hc < _hiddenSize.z; hc++) {
             int hiddenIndex = address3(Int3(pos.x, pos.y, hc), _hiddenSize);
 
-            float delta = _beta * (sigmoid(maxActivation) - sigmoid(activations[hc]));
+            float delta = _beta * (1.0f / _hiddenSize.z - (hc == maxIndex ? 1.0f : 0.0f));
 
             // For each visible layer
             for (int vli = 0; vli < _visibleLayers.size(); vli++) {
@@ -78,16 +78,12 @@ void ImageEncoder::learn(
 
     int visibleColumnIndex = address2(pos, Int2(vld._size.x, vld._size.y));
 
-    int targetC = (*inputActs[vli])[visibleColumnIndex];
-
     for (int vc = 0; vc < vld._size.z; vc++) {
         int visibleIndex = address3(Int3(pos.x, pos.y, vc), vld._size);
 
-        float target = (vc == targetC ? 1.0f : 0.0f);
-
         float sum = vl._weights.multiplyOHVsT(_hiddenCs, visibleIndex, _hiddenSize.z) / std::max(1, vl._weights.countT(visibleIndex) / _hiddenSize.z);
 
-        float delta = _alpha * (target - sigmoid(sum));
+        float delta = _alpha * ((*inputActs[vli])[visibleIndex] - sigmoid(sum));
 
         vl._weights.deltaOHVsT(_hiddenCs, delta, visibleIndex, _hiddenSize.z);
     }
