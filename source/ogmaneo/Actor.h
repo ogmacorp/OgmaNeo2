fba8dd4be8afba1ef94e831d22ba2e3c9bc490a7
Eric Laukien <ericlaukien@gmail.com>
2020-01-16 19:44:02 -0500

    Tuning

--- source/ogmaneo/Actor.h
+++ source/ogmaneo/Actor.h
@@ -109,10 +109,10 @@ public:
     // Defaults
     Actor()
     :
-    alpha(0.01f),
-    beta(0.01f),
+    alpha(0.005f),
+    beta(0.005f),
     gamma(0.99f),
-    historyIters(16)
+    historyIters(32)
     {}
 
     Actor(
