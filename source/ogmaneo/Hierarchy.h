183de3495acacb7436e1eb30e755135f0fda9ad8
Eric Laukien <ericlaukien@gmail.com>
2020-04-15 18:32:51 -0400

    Defaults

--- source/ogmaneo/Hierarchy.h
+++ source/ogmaneo/Hierarchy.h
@@ -58,7 +58,7 @@ public:
         ffRadius(2),
         pRadius(2),
         ticksPerUpdate(2),
-        temporalHorizon(2),
+        temporalHorizon(4),
         aRadius(2),
         historyCapacity(32)
         {}
