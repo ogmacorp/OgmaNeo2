18a579b766f761ba039f1f8c2feaaf44312702a7
Eric Laukien <ericlaukien@gmail.com>
2020-05-02 12:35:04 -0400

    Tests

--- source/ogmaneo/Actor.cpp
+++ source/ogmaneo/Actor.cpp
@@ -35,7 +35,7 @@ void Actor::forward(
 
     // --- Action ---
 
-    int maxIndex = 0;
+    std::vector<float> activations(hiddenSize.z);
     float maxActivation = -999999.0f;
 
     for (int hc = 0; hc < hiddenSize.z; hc++) {
@@ -51,13 +51,39 @@ void Actor::forward(
             sum += vl.actionWeights.multiplyOHVs(*inputCs[vli], hiddenIndex, vld.size.z);
         }
 
-        if (sum > maxActivation) {
-            maxActivation = sum;
-            maxIndex = hc;
+        sum /= std::max(1, count);
+
+        activations[hc] = sum;
+
+        maxActivation = std::max(maxActivation, sum);
+    }
+
+    float total = 0.0f;
+
+    for (int hc = 0; hc < hiddenSize.z; hc++) {
+        activations[hc] = std::exp(activations[hc] - maxActivation);
+        
+        total += activations[hc];
+    }
+
+    std::uniform_real_distribution<float> cuspDist(0.0f, total);
+
+    float cusp = cuspDist(rng);
+
+    int selectIndex = 0;
+    float sumSoFar = 0.0f;
+
+    for (int hc = 0; hc < hiddenSize.z; hc++) {
+        sumSoFar += activations[hc];
+
+        if (sumSoFar >= cusp) {
+            selectIndex = hc;
+
+            break;
         }
     }
     
-    hiddenCs[hiddenColumnIndex] = maxIndex;
+    hiddenCs[hiddenColumnIndex] = selectIndex;
 }
 
 void Actor::learn(
@@ -123,7 +149,7 @@ void Actor::learn(
 
         sum /= std::max(1, count);
 
-        float deltaAction = (mimic ? beta : (tdErrorAction > 0.0f ? beta : -beta)) * ((hc == targetC ? 1.0f : -1.0f) - std::tanh(sum));
+        float deltaAction = (mimic ? beta : beta * std::tanh(tdErrorAction)) * ((hc == targetC ? 1.0f : -1.0f) - std::tanh(sum));
 
         // For each visible layer
         for (int vli = 0; vli < visibleLayers.size(); vli++) {
